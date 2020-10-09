b1e236f5ea230457e758f9b039c72a9ff494135e
czuziak-jzmk <72632422+czuziak-jzmk@users.noreply.github.com>
2020-10-09 15:22:28 -0700

    Create SimpleSwitch.py

---
+++ SimpleSwitch.py
@@ -0,0 +1,14 @@
+# Load the Python Standard and DesignScript Libraries
+import sys
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# The inputs to this node will be stored as a list in the IN variables.
+dataEnteringNode = IN
+
+if IN[0]:
+	OUT = IN[1]
+
+if not IN[0]:
+	OUT = IN[2]
